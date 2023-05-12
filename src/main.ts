2301e18d0e50cd855bfb61574539a31498c911d6
陈海俊 <353376352@qq.com>
2023-05-12 22:53:09 +0800

    add certs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs from "node:fs";
+import path from "node:path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    httpsOptions: {
+      key: fs.readFileSync(path.join(__dirname, '../src/certs/traco.key')),
+      cert: fs.readFileSync(path.join(__dirname, '../src/certs/traco.pem'))
+    }
+  });
   app.enableCors();
   await app.listen(3100);
 }

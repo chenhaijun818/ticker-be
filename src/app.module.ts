313aa0b0451ba64b0fbe8f3ecb2629ca33c532cd
陈海俊 <353376352@qq.com>
2023-05-29 18:02:44 +0800

    修复.env路径问题

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { Todo, TodoSchema } from "./models/todo";
 import { HttpModule } from "@nestjs/axios";
 import { TokenInterceptor } from "./core/interceptors/token.interceptor";
 import { APP_INTERCEPTOR } from "@nestjs/core";
-import {ConfigModule} from "@nestjs/config"
+import { ConfigModule } from "@nestjs/config";
 import { User, UserSchema } from "./models/user";
-import {config} from 'dotenv';
-config({path: '.env'})
+import { config } from "dotenv";
+import { join } from "node:path";
+
+config({ path: join(__dirname, "..", ".env") });
+
 @Module({
   imports: [
     HttpModule,
